51b5d8912368bcb48ccfdc41af08267f1a93f45a
Jose Terrones <66368520+RadeonXYZ@users.noreply.github.com>
2021-05-16 20:15:46 -0500

    bst.h modificado

--- bst.h
+++ bst.h
@@ -1,18 +1,23 @@
-#ifndef __BST_H__ //Maizo Diego
-#define __BST_H__  //kevin de lama
+  
+#ifndef __BST_H__
+#define __BST_H__ 
+#include <iostream>
+using namespace std;
+
+//jose terrones
 
 template <typename T>
 class CBinaryTree
 {
     struct Node
     {
-      T data;//Maizo
+      T data;
       Node *m_pChild[2];
       Node(T &_data, Node *pLeft=nullptr, Node *pRight=nullptr)
         : data(_data)
       {   m_pChild[0] = pLeft;  m_pChild[1] = pRight;   }
-      T getData()             { return data; } //Maizo
-      void setData (T &_data) { data=_data; }  //Maizo
+      T getData()             { return data; } 
+      void setData (T &_data) { data=_data; }  
     };
 
     Node *m_pRoot = nullptr;
@@ -31,12 +36,21 @@ public:
     }
     void insert(T &dato)
     {   internal_insert(m_pRoot, dato);   }
+
+    void menor_a_mayor()
+    {   void recorrer(Node *&Reference1);  }
+
+    void mayor_a_menor()
+    {   void recorrer_inverso(Node *&Reference2);   }
+
 private:
-    void internal_insert(Node *&rParent,const T &dato);//Maizo
+    void internal_insert(Node *&rParent);
+    void recorrer(Node *&Reference1);
+    void recorrer_inverso(Node *&Reference2);
 };
 
 template <typename T>
-void CBinaryTree<T>:: internal_insert(Node *&rParent, T &dato) // kevin de lama
+void CBinaryTree<T>:: internal_insert(Node *&rParent, T &dato) 
 {
   if(!rParent)
   {   rParent = new Node(dato);
@@ -44,4 +58,37 @@ void CBinaryTree<T>:: internal_insert(Node *&rParent, T &dato) // kevin de lama
   }
   internal_insert(rParent->m_pChild[ dato > rParent->getData() ], dato);
 }
-#endif //Maizo Diego  
+
+template <typename T>
+void CBinaryTree<T>:: recorrer(Node *&Reference1)
+{
+  if (m_pRoot!=nullptr)
+  {
+    if(!Reference1)
+    {   
+      Reference1 = new Node;
+      return;
+    }
+    recorrer(Reference1->m_pChild[0]);
+    cout<<Reference1->getData();
+    recorrer(Reference1->m_pChild[1]);
+  }
+
+}
+
+template <typename T>
+void CBinaryTree<T>:: recorrer_inverso(Node *&Reference2)
+{
+  if (m_pRoot!=nullptr)
+  {
+    if(!Reference2)
+    {   
+      Reference2 = new Node;
+      return;
+    }
+    recorrer_inverso(Reference2->m_pChild[1]);
+    cout<<Reference2->getData();
+    recorrer_inverso(Reference2->m_pChild[0]);
+  }
+}
+#endif 
