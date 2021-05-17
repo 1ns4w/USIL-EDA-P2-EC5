8acd891b747ca92b9471eba21b061b4af32cfec9
Moises Moises <>
2021-05-17 04:20:21 +0000

    Recorridos mediante printDesc y printAsc funciones - Moisés Rivera

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include "bst.h"
+
+using namespace std;
+
+typedef int T1;
 
 int main()
 {
-  std::cout << "Hello World!\n";
+  CBinaryTree<T1>(obj);
+
+  T1 arr[] = {4, 30, 25, 20, 60, 10};
+
+  int len = sizeof(arr)/sizeof(arr[0]);
+
+  for (int i=0; i<len;i++) {
+    obj.insert(arr[i]);
+  }
+  obj.printDesc();
+  obj.printAsc();
 }
