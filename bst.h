721bc1179b2860eaac3598ecd82c7ba6c0a2799d
Joshua Christian Soria <74795522+josh18151@users.noreply.github.com>
2021-05-16 22:28:22 -0500

    Update bst.h

--- bst.h
+++ bst.h
@@ -1,5 +1,7 @@
 #ifndef __BST_H__ //Maizo Diego
 #define __BST_H__  //kevin de lama
+#include<conio.h>
+#include<string.h>
 
 template <typename T>
 class CBinaryTree
@@ -16,6 +18,122 @@ class CBinaryTree
     };
 
     Node *m_pRoot = nullptr;
+    struct node
+  {
+   int n;
+   struct node*sgte;
+   };
+   typedef struct node*Tlista;
+   void inserta(Tlista &,int);
+   void eliminar_inicio(Tlista &);
+   void eliminar_fin(Tlista &);
+   void eliminar_elemento(Tlista &,int);
+   void eliminar_lista(Tlista &);
+   void media(Tlista );
+   void busqueda(Tlista, int);
+   void eliminarepetidos(Tlista &);
+   void unelista(Tlista &,Tlista &,Tlista &);
+   void mayorlista(Tlista );
+   void menorlista(Tlista);
+   void eliminarpares(Tlista &);
+   void numerosprimos(Tlista);
+   void ordenaburbuja(Tlista);
+   void imprimeinver(Tlista);
+   void imprimir(Tlista);
+    void imprimir(Tlista lista)
+ {
+  if(lista==NULL)
+  cout<<"ingresa elementos";
+  while(lista!=NULL)
+  {
+   cout<<lista->n<<endl;
+   lista=lista->sgte;
+   }
+   cout<<endl;
+   }
+void imprimeinver(Tlista lista)
+{
+ int x[20],k=0;
+ if(lista!=NULL)
+ cout<<"lista vacia, ingrese elementos";
+ while(lista!=NULL)
+ {
+  lista=lista->sgte;
+   x[k]=lista->n;
+    for(int i=0;i>=0;i--)
+    cout<<x[i];
+    }
+    }
+void inserta(Tlista &lista,int k)
+ {
+  int i;
+  Tlista q;
+  for(i=0;i<k;i++)
+   {
+    q=new(struct node);
+    cout<<"nt ELEMENTO ["<<i<<"] :";
+    cin>>q->n;
+    q->sgte=lista;
+    lista=q;
+    }
+    }
+void eliminar_inicio(Tlista &lista)
+ {
+  struct node*p;
+  if(lista!=NULL)
+    {
+     p=lista;
+     lista=lista->sgte;
+     delete(p);
+     }
+     }
+void eliminar_fin(Tlista &lista)
+  {
+   Tlista p,ant;
+   if(lista==NULL) return;
+   p=lista;
+   if(lista->sgte==NULL)
+   lista=lista->sgte;
+   else
+   {
+   while(p->sgte!=NULL)
+   {
+   ant=p;
+   p=p->sgte;
+   }
+   ant->sgte=NULL;
+   }
+   delete(p);
+  }
+void eliminar_elemento(Tlista &lista,int valor)
+  {
+   Tlista p,ant;
+   p=lista;
+  while(p!=NULL)
+  {
+  if(p->n==valor)
+  {
+  if(p==lista)
+  lista=lista->sgte;
+  else
+  ant->sgte=p->sgte;
+  delete(p);
+  return;
+  }
+  ant=p;
+  p=p->sgte;
+  }
+  }
+void elimina_lista(Tlista &lista)
+  {
+  Tlista p;
+  while(lista!=NULL)
+  {
+  p=lista;
+  lista=lista->sgte;
+  delete(p);
+  }
+  }
 public:
     CBinaryTree() {}
     void insert_old(const T &dato)
