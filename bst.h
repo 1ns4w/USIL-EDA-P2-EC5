25e7df8fb7f9ad6802cae1b59df5c9b329ddbb22
Joshua Christian Soria <74795522+josh18151@users.noreply.github.com>
2021-05-16 21:27:12 -0500

    Update bst.h

--- bst.h
+++ bst.h
@@ -1,5 +1,10 @@
 #ifndef __BST_H__ //Maizo Diego
 #define __BST_H__  //kevin de lama
+#include <iostream>
+#include <algorithm>
+#include <string.h>
+int main()
+using namespace std; 
 
 template <typename T>
 class CBinaryTree
@@ -44,4 +49,52 @@ void CBinaryTree<T>:: internal_insert(Node *&rParent, T &dato) // kevin de lama
   }
   internal_insert(rParent->m_pChild[ dato > rParent->getData() ], dato);
 }
+{
+    int matrix[12][12];
+    std::generate(matrix, matrix + 12*12,
+                  [](){ return 2*(rand() % 256+1); });
+    auto min_max =std::minmax_element(matrix, matrix + 12*12);
+    std::cout << *min_max.first << ", " << *min_max.second << std::endl;
+}
+struct ordenamiento
+    {int elem ;
+    };
+void leerarray(int,struct ordenamiento []);
+void burbuja (int,struct ordenamiento []);
+void muestra (int,struct ordenamiento []);
+void main()
+{
+    int n;
+    struct ordenamiento ordenada [MAX];
+    cout <<"limite de arreglo:";
+    cin>>n;
+    leerarray (n,ordenada);
+    burbuja (n,ordenada);
+    cout<< endl << "arreglo ordenado: " << endl;
+    muestra(n,ordenada);
+    getch();
+}
+void leerarray(int n,struct ordenamiento a [])
+{for (int i=0;i<n;i++)
+}
+cout<<"elemento" <<i<<":";
+cin>>a[i].elem;
+}
+void burbuja (int n,struct ordenamiento a [])
+{
+    int i,j;
+    struct ordenamiento temp;
+    for(i=1;i<n;i++)
+    for(j=n-1;j>=i;j--)
+        if(a[j-1].elem>a[j].elem)
+        {
+            temp=a[j-1];
+            a[j-1]=a[j];
+            a[j]=temp;
+        }
+ void muestra(int n,struct ordenamiento a[])
+ {
+     for(int i=0;<n;i++)
+         cout<<" " <<a[i].elem;
+ }       
 #endif //Maizo Diego  
